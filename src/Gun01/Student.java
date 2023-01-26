22583417475070bf67b43c62679344cebb321f88
ahmet <ahmetersz@gmail.com>
2023-01-26 19:23:34 +0300

    ilk task yapıldı

---
+++ src/Gun01/Student.java
@@ -0,0 +1,12 @@
+package Gun01;
+
+public class Student {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Merhaba Cimbombom");
+
+        }
+
+    }
+}
